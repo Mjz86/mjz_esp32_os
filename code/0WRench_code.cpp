35410d31be71eb67f658c6995b0533c3d0a7537a
Mjz86 <112626877+Mjz86@users.noreply.github.com>
2023-09-20 17:59:53 +0330

    Update 0WRench_code.cpp

--- code/0WRench_code.cpp
+++ code/0WRench_code.cpp
@@ -59,21 +59,18 @@ const char* WRENCH_code_path_pair_array_dis[NUMber_of_WRENCH_code_path_pair_dis_
   { "/example/dis/var.wr", R"lgkekuf(display_fillScreen(0);display_setCursor(0,0);display_setTextSize(1);display_setTextColor(display_RGB_TO_color(255,255,255));display_setTextWrap(true);display_print(get_VAR());display_display();)lgkekuf" },
   { "/example/dis/clear.wr", R"lgkekuf(display_fillScreen(0);display_display();)lgkekuf" },
   { "/bin/boot.bin", R"lgkekuf(mjz_change_my_NeoPixel(1,48);for(j=0;j<360;j++){mjz_set_pixel_colorhsv(0,j);mjz_pixel_show();delay(20);}mjz_pixel_clear();mjz_pixel_show();
- TFT_BLK_pwm_chanel =15;TFT_BLK =14;TFT_BLK_pwm_frequency =312500;
- btn_cntrl = 21;inputmode = 1;pinMode(btn_cntrl,inputmode);
- for(;;){sigmaDeltaWrite(TFT_BLK,TFT_BLK_pwm_chanel,TFT_BLK_pwm_frequency,255*digitalRead(btn_cntrl));delay(20);}
- )lgkekuf" },
-  { "/main.bin", R"lgkekuf(pinMode(btn_digital_pin,inputpulldownmode);pinMode(x_analog_pin,inputmode);pinMode(y_analog_pin,inputmode); x_analog_pin = 7;y_analog_pin = 6;btn_digital_pin = 0;inputmode = 1;inputpulldownmode = 5;
-  for(;;){display_fillScreen(display_RGB_TO_color(255,255,255));display_display();for(k =0;k<360;k++) {for (i = 0;i< 7;i++){for (j = 0;j< 6;j++){x = j*40;y = i*40;
- display_fillRoundRect(x ,y ,40,40,5,HSVTODISCLR(j*60-i*5+k));display_setCursor(x+15-(i*2),y+15-(j*2));display_setTextSize(2);display_setTextColor(0);display_print(":)");}
- x_position = (analogRead(x_analog_pin) *240)/4095;y_position = 280-((analogRead(y_analog_pin)*280)/4095);button_is_active = !digitalRead(btn_digital_pin);
- display_fillCircle(x_position,y_position,10,display_RGB_TO_color(255));if(button_is_active){display_fillCircle(x_position,y_position,5,0);
- display_display();delay(0);}}}} )lgkekuf" },
+ TFT_BLK =14;btn_cntrl = 21;inputmode = 1;outmode = 3;pinMode(btn_cntrl,inputmode);pinMode(TFT_BLK,outmode);
+ for(;;){digitalWrite(TFT_BLK,digitalRead(btn_cntrl));delay(20);})lgkekuf" },
+
+  { "/main.bin", R"lgkekuf(x_analog_pin = 7; y_analog_pin = 6; btn_digital_pin = 0; inputmode = 1; INPUT_PULLUP = 5; pinMode(btn_digital_pin,INPUT_PULLUP);pinMode(x_analog_pin,inputmode);pinMode(y_analog_pin,inputmode);
+ for(;;){display_fillScreen(display_RGB_TO_color(255,255,255)); for(k =0;k<360;k++) {for (i = 0;i< 7;i++){for (j = 0;j< 6;j++){x = j*40;y = i*40; display_fillRoundRect(x ,y ,40,40,5,HSVTODISCLR(j*60-i*5+k)); display_setCursor(x+15-(i*2),y+15-(j*2)); display_setTextSize(2); display_setTextColor(0); display_print(":)"); 
+  x_position = (analogRead(x_analog_pin) *240)/4095; y_position = 280-((analogRead(y_analog_pin)*280)/4095); button_is_active = !digitalRead(btn_digital_pin); display_fillCircle(x_position,y_position,10,display_RGB_TO_color(255)); 
+ if(button_is_active){display_fillCircle(x_position,y_position,5,0);}display_display(); delay(1); }}}})lgkekuf" },
 
 
   { "/bin/main.bin", R"lgkekuf(for(;;){display_fillScreen(display_RGB_TO_color(0,0,0));
- x_analog_pin = 7;y_analog_pin = 6;btn_digital_pin = 0;inputmode = 1;inputpulldownmode = 5;
- pinMode(btn_digital_pin,inputpulldownmode);pinMode(x_analog_pin,inputmode);pinMode(y_analog_pin,inputmode);
+ x_analog_pin = 7;y_analog_pin = 6;btn_digital_pin = 0;inputmode = 1;INPUT_PULLUP = 5;
+ pinMode(btn_digital_pin,INPUT_PULLUP);pinMode(x_analog_pin,inputmode);pinMode(y_analog_pin,inputmode);
  x_position = analogRead(x_analog_pin);y_position = analogRead(y_analog_pin);button_is_active = !digitalRead(btn_digital_pin);
  y_position = 280-(y_position*280/4095);
  x_position =x_position*240/4095;
